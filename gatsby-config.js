ed71e34d51eaa1a40faec48923b1bfb661e001ab
Shayan Bemanian <shayan.bemanian@yahoo.com>
2021-07-22 14:26:45 +0430

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,31 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
-    description: "An awesome Gatsby-Wordpress boilerplate.",
-    author: "Shayan Bemanian",
+    title: 'My Gatsby Site',
+    description: 'An awesome Gatsby-Wordpress boilerplate.',
+    author: 'Shayan Bemanian',
   },
   plugins: [
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
         url: `${process.env.WORDPRESS_GQL_URL}/graphql`,
-        // Uncomment this if needed
-        // schema: {
-        //   requestConcurrency: 5,
-        //   previewRequestConcurrency: 2,
-        // },
       },
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
+        name: 'images',
         path: `${__dirname}/src/dist/images/`,
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
-};
+}
